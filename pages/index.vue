b7ed987d99daf184f2f4fd20780c134f96cb15d1
Sahar Hage <saharhage9@gmail.com>
2022-11-18 11:58:57 +0200

    💄 Global css resets

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,37 @@
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+body,
+html {
+  font-size: $font-size-root !important;
+  margin: 0;
+  padding: 0;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+a,
+button {
+  margin: 0;
+  padding: 0;
+}
+
+button {
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+}
+a,
+a:hover {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
